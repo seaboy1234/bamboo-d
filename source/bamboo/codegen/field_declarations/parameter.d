e4ff70904a13a7bfa3552cf196b9d2b0bc2ffaa3
Christian Wilson <contact@christianwilson.me>
2018-07-27 16:44:31 -0700

    Rewrite codegen for parameter fields.

--- source/bamboo/codegen/field_declarations/parameter.d
+++ source/bamboo/codegen/field_declarations/parameter.d
@@ -1,42 +1,36 @@
 module bamboo.codegen.field_declarations.parameter;
 
+import std.format;
+
 import bamboo.codegen.field_declarations;
 
+private enum parameterFieldStubbed = q{
+    void %1$s (%2$s value) @FieldId(%3$s) %4$-(@%s %) @property
+    {
+        _%1$s = value;
+    }
+    inout(%2$s) %1$s() inout @property
+    {
+        return _%1$s;
+    }
+};
 
+private enum parameterFieldAbstract = q{
+    abstract void %1$s(%2$s value) @FieldId(%3$s) %4$-(@%s %) @property;
+    abstract %2$s %1$s() @property;
+};
 
 string generateParameterField(ParameterField field, bool stub)
 {
-    string format;
+    string code;
     string def = generateDefinition(field.parameter);
     string type = def.split(' ')[0];
     string name = def.split(' ')[1];
 
-    format ~= "@FieldId(" ~ field.id.to!string ~ ") ";
-    format ~= "@FieldType!(" ~ generateDefinition(field.parameter).split(' ')[0] ~ ") ";
-
-    foreach (keyword; field.keywords)
-    {
-        format ~= " @" ~ keyword ~ " ";
-    }
-
     if (!stub)
     {
-        format ~= "abstract void " ~ name ~ "(" ~ type ~ " value) @property;";
-        format ~= "abstract " ~ def ~ "();";
+        return parameterFieldAbstract.format(name, type, field.id, field.keywords.keywords);
     }
-    else
-    {
-        format ~= "void " ~ name ~ "(" ~ type ~ " value) @property {";
-        format ~= "_" ~ name ~ " = value;";
-        format ~= "}";
-
-        format ~= "@FieldId(" ~ field.id.to!string ~ ") ";
-
-        format ~= type ~ " " ~ name ~ "() inout @property {";
-        format ~= "return _" ~ name ~ ";";
-        format ~= "}";
-        format ~= type ~ " _" ~ name ~ ";";
-    }
-
-    return format;
+    
+    return parameterFieldStubbed.format(name, type, field.id, field.keywords.keywords);
 }
