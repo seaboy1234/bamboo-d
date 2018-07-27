125a7a65c04a6a147c3aa5b04a2249112c412a0f
Christian Wilson <contact@christianwilson.me>
2018-07-27 16:52:53 -0700

    Additional fixes in parameter field codegen.

--- source/bamboo/codegen/field_declarations/parameter.d
+++ source/bamboo/codegen/field_declarations/parameter.d
@@ -5,11 +5,12 @@ import std.format;
 import bamboo.codegen.field_declarations;
 
 private enum parameterFieldStubbed = q{
+    private %2$s _%1$s;
     void %1$s (%2$s value) @FieldId(%3$s) %4$-(@%s %) @property
     {
         _%1$s = value;
     }
-    inout(%2$s) %1$s() inout @property
+    inout(%2$s) %1$s() inout @FieldId(%3$s) @property
     {
         return _%1$s;
     }
@@ -17,12 +18,11 @@ private enum parameterFieldStubbed = q{
 
 private enum parameterFieldAbstract = q{
     abstract void %1$s(%2$s value) @FieldId(%3$s) %4$-(@%s %) @property;
-    abstract %2$s %1$s() @property;
+    abstract %2$s %1$s() @FieldId(%3$s) @property;
 };
 
 string generateParameterField(ParameterField field, bool stub)
 {
-    string code;
     string def = generateDefinition(field.parameter);
     string type = def.split(' ')[0];
     string name = def.split(' ')[1];
